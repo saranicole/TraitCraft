3774f6febd4667d7f6c0613db411d8c8c2891175
Sara Jarjoura <saranicole1980+github@gmail.com>
2025-08-10 00:24:34 -0400

    Get all known traits and use bitwise masks to store char knowledge

--- TraitCraft/TraitCraft.lua
+++ TraitCraft/TraitCraft.lua
@@ -14,11 +14,12 @@ TC.Default = {
     woodworkingCharacter = {},
     jewelryCharacter = {},
     activelyResearchingCharacters = {},
-    limitTraitsSaved = true,
+    traitTable = {},
 }
 
 TC.currentlyLoggedInCharId = TC.currentlyLoggedInCharId or GetCurrentCharacterId()
 TC.currentlyLoggedInChar = TC.currentlyLoggedInChar or {}
+TC.bitwiseChars = TC.bitwiseChars or {}
 local currentlyLoggedInCharId = TC.currentlyLoggedInCharId
 local currentlyLoggedInChar = {}
 local researchLineId = nil
@@ -34,12 +35,24 @@ if IsInGamepadPreferredMode() then
   SMITHING = ZO_GamepadSmithingResearch
 end
 
+
+function TC.GetCharacterBitwise()
+  local characterList = {}
+  for i = 1, GetNumCharacters() do
+      local name, _, _, _, _, backupId, id = GetCharacterInfo(i)
+      characterList[id or backupId] = 2^(i-1)
+  end
+  return characterList
+end
+
 --When Loaded
 local function OnAddOnLoaded(eventCode, addonName)
   if addonName ~= TC.Name then return end
 	EVENT_MANAGER:UnregisterForEvent(TC.Name, EVENT_ADD_ON_LOADED)
 
   TC.AV = ZO_SavedVars:NewAccountWide("TraitCraft_Vars", 1, nil, TC.Default)
+
+  TC.bitwiseChars = TC.GetCharacterBitwise()
 end
 
 function TC.isValueInTable(table, element)
@@ -64,48 +77,84 @@ local function getValueFromTable(t)
     return select(2, next(t))
 end
 
+local function HasResearched(trait, mask)
+    -- traitFlags is the integer bitmask
+    -- mask is the power-of-two flag for the character (e.g., 1, 2, 4, 8, ...)
+    return (trait % (mask * 2)) >= mask
+end
+
 local function TC_Event_Player_Activated(event, isA)
 	--Only fire once after login!
 	EVENT_MANAGER:UnregisterForEvent("TC_PLAYER_ACTIVATED", EVENT_PLAYER_ACTIVATED)
 	TC.currentlyLoggedInChar = {}
 	TC.BuildMenu()
 	if TC.AV.activelyResearchingCharacters[currentlyLoggedInCharId] then
-    TC.AV.activelyResearchingCharacters[currentlyLoggedInCharId].unknownTraits = {}
-    TraitCraft:ScanUnknownTraits()
+    -- TC.AV.activelyResearchingCharacters[currentlyLoggedInCharId].unknownTraits = {}
+    TraitCraft:ScanKnownTraits()
   end
-  if next(TC.AV.allCrafterIds) then
-    if TC.isValueInTable(TC.AV.allCrafterIds, currentlyLoggedInCharId) and getValueFromTable(TC.AV.activelyResearchingCharacters).unknownTraits then
-      TraitCraft:ScanIntersectingKnownTraitsOnCrafter()
-    end
-  end
+--   if next(TC.AV.allCrafterIds) then
+--     if TC.isValueInTable(TC.AV.allCrafterIds, currentlyLoggedInCharId) and getValueFromTable(TC.AV.activelyResearchingCharacters).unknownTraits then
+--       TraitCraft:ScanIntersectingKnownTraitsOnCrafter()
+--     end
+--   end
+end
+
+function TraitCraft:GetTraitKey(craftingSkillType, researchLineIndex, traitIndex)
+	if craftingSkillType == nil or researchLineIndex == nil or traitIndex == nil then return end
+	return craftingSkillType * 10000 + researchLineIndex * 100 + traitIndex
 end
 
 local function AddAltNeedIcon(control, craftingType, researchLineIndex, traitIndex)
     local specificIcon = nil
     local sideFloat = 180
-    for id, value in pairs(TC.AV.activelyResearchingCharacters) do
-      if TC.AV.sharedCrafterVars[TC.currentlyLoggedInCharId] and TC.AV.sharedCrafterVars[TC.currentlyLoggedInCharId][id] then
-        local altNeeds = TC.AV.sharedCrafterVars[TC.currentlyLoggedInCharId][id][craftingType]
-        if altNeeds and altNeeds[researchLineIndex] and altNeeds[researchLineIndex][traitIndex] then
-            if not control.altNeedIcon then
-                control.altNeedIcon = {}
-            end
-            if not control.altNeedIcon[id] then
-                local icon = WINDOW_MANAGER:CreateControl("iconId"..id.."C"..craftingType.."R"..researchLineIndex.."T"..traitIndex, control, CT_TEXTURE)
-                icon:SetDimensions(40, 40)
-                icon:SetAnchor(RIGHT, control, RIGHT, sideFloat, 0)
-                icon:SetTexture(value.icon)
-                control.altNeedIcon[id] = icon
-            end
-            if control.altNeedIcon[id] then
-              control.altNeedIcon[id]:SetHidden(false)
-              sideFloat = sideFloat + 40
-            end
+    local key = TraitCraft:GetTraitKey(craftingSkillType, researchLineIndex, traitIndex)
+    local trait = TC.AV.traitTable[key] or 0
+    for id, mask in pairs(TC.bitwiseChars) do
+      if TC.AV.activelyResearchingCharacters[id] then
+        local iconPath = TC.AV.activelyResearchingCharacters[id].icon
+        if not HasResearched(trait, mask) then
+              if not control.altNeedIcon then
+                  control.altNeedIcon = {}
+              end
+              if not control.altNeedIcon[id] then
+                  local icon = WINDOW_MANAGER:CreateControl("iconId"..id.."C"..craftingType.."R"..researchLineIndex.."T"..traitIndex, control, CT_TEXTURE)
+                  icon:SetDimensions(40, 40)
+                  icon:SetAnchor(RIGHT, control, RIGHT, sideFloat, 0)
+                  icon:SetTexture(iconPath)
+                  control.altNeedIcon[id] = icon
+              end
+              if control.altNeedIcon[id] then
+                control.altNeedIcon[id]:SetHidden(false)
+                sideFloat = sideFloat + 40
+              end
         elseif control.altNeedIcon and control.altNeedIcon[id] then
           control.altNeedIcon[id]:SetHidden(true)
         end
       end
     end
+--     for id, value in pairs(TC.AV.activelyResearchingCharacters) do
+--       if TC.AV.sharedCrafterVars[TC.currentlyLoggedInCharId] and TC.AV.sharedCrafterVars[TC.currentlyLoggedInCharId][id] then
+--         local altNeeds = TC.AV.sharedCrafterVars[TC.currentlyLoggedInCharId][id][craftingType]
+--         if altNeeds and altNeeds[researchLineIndex] and altNeeds[researchLineIndex][traitIndex] then
+--             if not control.altNeedIcon then
+--                 control.altNeedIcon = {}
+--             end
+--             if not control.altNeedIcon[id] then
+--                 local icon = WINDOW_MANAGER:CreateControl("iconId"..id.."C"..craftingType.."R"..researchLineIndex.."T"..traitIndex, control, CT_TEXTURE)
+--                 icon:SetDimensions(40, 40)
+--                 icon:SetAnchor(RIGHT, control, RIGHT, sideFloat, 0)
+--                 icon:SetTexture(value.icon)
+--                 control.altNeedIcon[id] = icon
+--             end
+--             if control.altNeedIcon[id] then
+--               control.altNeedIcon[id]:SetHidden(false)
+--               sideFloat = sideFloat + 40
+--             end
+--         elseif control.altNeedIcon and control.altNeedIcon[id] then
+--           control.altNeedIcon[id]:SetHidden(true)
+--         end
+--       end
+--     end
 end
 
 local function addSmithingHook()
@@ -118,18 +167,13 @@ end
 
 local function OnCraftingInteract(eventCode, craftingType)
   if next(TC.AV.allCrafterIds) then
-    if TC.isValueInTable(TC.AV.allCrafterIds, currentlyLoggedInCharId) and getValueFromTable(TC.AV.activelyResearchingCharacters).unknownTraits then
+    if TC.isValueInTable(TC.AV.allCrafterIds, currentlyLoggedInCharId) then
       ZO_PreHook(SMITHING, "ShowTraitsFor", function(self, data)
         researchLineId = data.researchLineIndex
         addSmithingHook()
       end)
     end
   end
-end
-
-function TraitCraft:GetTraitKey(craftingSkillType, researchLineIndex, traitIndex)
-	if craftingSkillType == nil or researchLineIndex == nil or traitIndex == nil then return end
-	return craftingSkillType * 10000 + researchLineIndex * 100 + traitIndex
 end
 
 function TraitCraft:GetTraitFromKey(key)
@@ -221,37 +265,64 @@ function TraitCraft:ScanIntersectingKnownTraitsOnCrafter()
   end
 end
 
-function TraitCraft:ScanUnknownTraits()
+local function needsToBeAdded(trait, mask)
+  return (trait % (mask*2)) < mask
+end
+
+function TraitCraft:ScanKnownTraits()
   local char = TC.AV.activelyResearchingCharacters[currentlyLoggedInCharId]
   local traitLimit = 9
-  if TC.AV.limitTraitsSaved then
-    traitLimit = 1
-  end
+  local charBitId = TC.bitwiseChars[currentlyLoggedInCharId]
+  local key = nil
 	for researchLineIndex = 1, GetNumSmithingResearchLines(BLACKSMITH) do
 		for traitIndex = 1, traitLimit do
-      if not self:WillCharacterKnowTrait(BLACKSMITH, researchLineIndex, traitIndex) then
-        table.insert(char.unknownTraits, self:GetTraitKey(BLACKSMITH, researchLineIndex, traitIndex))
+      if self:WillCharacterKnowTrait(BLACKSMITH, researchLineIndex, traitIndex) then
+        key = self:GetTraitKey(BLACKSMITH, researchLineIndex, traitIndex)
+        if not TC.AV.traitTable[key] then
+          TC.AV.traitTable[key] = 0
+        end
+        if needsToBeAdded(TC.AV.traitTable[key], charBitId) then
+          TC.AV.traitTable[key] = TC.AV.traitTable[key] + charBitId
+        end
       end
 		end
 	end
 	for researchLineIndex = 1, GetNumSmithingResearchLines(CLOTHIER) do
     for traitIndex = 1, traitLimit do
-      if not self:WillCharacterKnowTrait(CLOTHIER, researchLineIndex, traitIndex) then
-        table.insert(char.unknownTraits, self:GetTraitKey(CLOTHIER, researchLineIndex, traitIndex))
+      if self:WillCharacterKnowTrait(CLOTHIER, researchLineIndex, traitIndex) then
+        key = self:GetTraitKey(CLOTHIER, researchLineIndex, traitIndex)
+        if not TC.AV.traitTable[key] then
+          TC.AV.traitTable[key] = 0
+        end
+        if needsToBeAdded(TC.AV.traitTable[key], charBitId) then
+          TC.AV.traitTable[key] = TC.AV.traitTable[key] + charBitId
+        end
       end
     end
 	end
 	for researchLineIndex = 1, GetNumSmithingResearchLines(WOODWORK) do
     for traitIndex = 1, traitLimit do
-      if not self:WillCharacterKnowTrait(WOODWORK, researchLineIndex, traitIndex) then
-        table.insert(char.unknownTraits, self:GetTraitKey(WOODWORK, researchLineIndex, traitIndex))
+      if self:WillCharacterKnowTrait(WOODWORK, researchLineIndex, traitIndex) then
+        key = self:GetTraitKey(WOODWORK, researchLineIndex, traitIndex)
+        if not TC.AV.traitTable[key] then
+          TC.AV.traitTable[key] = 0
+        end
+        if needsToBeAdded(TC.AV.traitTable[key], charBitId) then
+          TC.AV.traitTable[key] = TC.AV.traitTable[key] + charBitId
+        end
       end
     end
 	end
 	for researchLineIndex = 1, GetNumSmithingResearchLines(JEWELRY_CRAFTING) do
     for traitIndex = 1, traitLimit do
-      if not self:WillCharacterKnowTrait(JEWELRY_CRAFTING, researchLineIndex, traitIndex) then
-        table.insert(char.unknownTraits, self:GetTraitKey(JEWELRY_CRAFTING, researchLineIndex, traitIndex))
+      if self:WillCharacterKnowTrait(JEWELRY_CRAFTING, researchLineIndex, traitIndex) then
+        key = self:GetTraitKey(JEWELRY_CRAFTING, researchLineIndex, traitIndex)
+        if not TC.AV.traitTable[key] then
+          TC.AV.traitTable[key] = 0
+        end
+        if needsToBeAdded(TC.AV.traitTable[key], charBitId) then
+          TC.AV.traitTable[key] = TC.AV.traitTable[key] + charBitId
+        end
       end
     end
 	end
