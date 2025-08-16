fa72fbd1f7e25b3d88e7022e22998b6cefe0b34a
Sara Jarjoura <saranicole1980+github@gmail.com>
2025-08-15 23:33:35 -0400

    Add espanol

---
+++ TraitCraft/lang/es.lua
@@ -0,0 +1,18 @@
+local TC = TraitCraft
+
+TC.Lang = {
+  MAIN_CRAFTER            =         "Artesano principal",
+  SET_ALL_TO_MAIN_CRAFTER =         "Establecer todo en el artesano principal",
+  BLACKSMITHING_CHARACTER =         "Artesano herrero",
+  CLOTHING_CHARACTER      =         "Artesano de ropa",
+  WOODWORKING_CHARACTER   =         "Artesano de la carpintería",
+  JEWELRY_CHARACTER       =         "Artesano de joyería",
+  SELECT_ACTIVELY_RESEARCHING    =  "Selecciona personajes que estén investigando activamente",
+  ACTIVE_APPLY            =         "Aplicar configuración",
+  CHARACTER_ICON          =         "Icono de personaje",
+  STATUS_ADDED            =         "¡Configurado!",
+  ACTIVELY_RESEARCHING    =         "Investigando activamente personajes",
+  CLEAR_ACTIVELY_RESEARCHING =      "Reiniciar personajes que están investigando",
+  SHORT_CLEAR             =         "Reiniciar",
+
+}
