aefc32a60edfbfdff7986386ad74422937a75e91
Sara Jarjoura <saranicole1980+github@gmail.com>
2025-08-18 23:11:51 -0400

    Try a counter

--- TraitCraft/Crafting.lua
+++ TraitCraft/Crafting.lua
@@ -56,8 +56,11 @@ local function OnSmithingCreation(eventCode, craftingType)
             if icon and researchLineIndex and traitIndex and selectedTrait ~= 0 then
               TC.sideFloat[icon:GetName()] = 10
               local charId = TC.AddAltNeedIcon(icon, nil, craftingType, researchLineIndex, traitIndex, TOP, BOTTOM, TC.sideFloat[icon:GetName()], "craftId")
+              local counter = 1
               for id, value in pairs(TC.AV.activelyResearchingCharacters) do
+                TC.sideFloat[icon:GetName()] = 10 + 40 * counter
                 TC.AddAltNeedIcon(icon, id, craftingType, researchLineIndex, traitIndex, TOP, BOTTOM, TC.sideFloat[icon:GetName()], "craftId")
+                counter = counter + 1
               end
             end
           end
