472edc5b43d9f7fcdc97a3aa5f2f3e3633478639
Sara Jarjoura <saranicole1980+github@gmail.com>
2025-08-18 23:15:39 -0400

    Better

--- TraitCraft/Crafting.lua
+++ TraitCraft/Crafting.lua
@@ -56,7 +56,7 @@ local function OnSmithingCreation(eventCode, craftingType)
             if icon and researchLineIndex and traitIndex and selectedTrait ~= 0 then
               TC.sideFloat[icon:GetName()] = 10
               local charId = TC.AddAltNeedIcon(icon, nil, craftingType, researchLineIndex, traitIndex, TOP, BOTTOM, TC.sideFloat[icon:GetName()], "craftId")
-              local counter = 1
+              local counter = 0
               for id, value in pairs(TC.AV.activelyResearchingCharacters) do
                 TC.sideFloat[icon:GetName()] = 10 + 40 * counter
                 TC.AddAltNeedIcon(icon, id, craftingType, researchLineIndex, traitIndex, TOP, BOTTOM, TC.sideFloat[icon:GetName()], "craftId")
