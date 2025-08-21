12d8daca9a72a447463f7622f889f52be6a38d8b
Sara Jarjoura <saranicole1980+github@gmail.com>
2025-08-20 22:47:58 -0400

    Use function lock

--- TraitCraft/Crafting.lua
+++ TraitCraft/Crafting.lua
@@ -54,7 +54,7 @@ local function OnSmithingCreation(eventCode, craftingType)
             local researchLineIndex = findResearchLineIndex(craftingType, self.patternList.selectedData.patternName)
             local traitIndex = findTraitIndex(craftingType, researchLineIndex, selectedTrait)
             if icon and researchLineIndex and traitIndex and selectedTrait ~= 0 then
-              TC.AddAltNeedIcon(icon, nil, craftingType, researchLineIndex, traitIndex, TOP, BOTTOM, 0, "craftId")
+              TC.AddAltNeedIcon(icon, craftingType, researchLineIndex, traitIndex, TOP, BOTTOM, 0, "craftId")
             end
           end
         end)
