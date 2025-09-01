f72869e33f3361277ea0ffac0211d6ec57ff5f74
Sara Jarjoura <saranicole1980@gmail.com>
2025-09-01 18:28:13 -0400

    fix(autocraft) Fix settings reference

--- TraitCraft/TraitCraft.lua
+++ TraitCraft/TraitCraft.lua
@@ -372,7 +372,7 @@ local function TC_Event_Player_Activated(event, isA)
   if IsConsoleUI() then
     TC.inventory = TC_Inventory:New(TC)
   end
-  if GetDisplayName() == "@Saranicole1980" and LibLazyCrafting and TC.settings.autoCraftOption then
+  if GetDisplayName() == "@Saranicole1980" and LibLazyCrafting and TC.AV.settings.autoCraftOption then
     TC.autocraft = TC_Autocraft:New(TC)
   end
   local FIVE_MINUTES_MS = 5 * 60 * 1000  -- 5 min in ms
